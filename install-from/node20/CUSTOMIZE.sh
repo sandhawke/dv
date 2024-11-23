f2099f406992e374bab99ed608bf56c162401d95
Sandro Hawke <sandro@hawke.org>
2024-11-23 14:59:32 -0500

    this is node specific

--- install-from/node20/CUSTOMIZE.sh
+++ install-from/node20/CUSTOMIZE.sh
@@ -4,3 +4,9 @@
 #
 # But that means the AI is seeing your name, which make you
 # don't want. Hrm.
+
+echo 'node_modules' >> .gitignore
+
+echo '#include ".gitignore"' >> .dv/nopack.txt
+echo 'package-lock.json' >> .dv/nopack.txt
+
