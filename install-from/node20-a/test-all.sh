c1b6c4ffc654a4b4481b827585853a0b5d225a0a
Sandro Hawke <sandro@hawke.org>
2024-11-25 00:20:30 -0500

    clean up test-all logic I think

--- install-from/node20-a/test-all.sh
+++ install-from/node20-a/test-all.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Edit this file to add the commands for your environment which are
+# needed to build/rebuild and test the software in this project.
+# Assume it will be run as "bash test-all.sh". This file should exit
+# with status 0 if and only if all tests pass.
+
+# We are using node.js so we need these:
 npm install
 npm test
+
+# Also our command-line tests, see ./cltest/README 
+dv-cltest ./cltest/[a-z]*.sh
