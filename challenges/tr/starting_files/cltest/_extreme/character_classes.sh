84010a9d5e248559a1a47fb0502dda3f7559158e
Sandro Hawke <sandro@hawke.org>
2024-11-28 09:52:32 -0500

    delete _extreme challenge tests

--- challenges/tr/starting_files/cltest/_extreme/character_classes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source "$PROJECT_ROOT/cltest/_common.sh"
-
-# Test POSIX character classes
-echo "hello123" | $COMMAND '[:digit:]' '0' > output.txt
-assert_output "hello000" "$(cat output.txt)"
--- challenges/wc/starting_files/cltest/_extreme/_handle_invalid_option.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Test invalid option handling
-$COMMAND -z "$PROJECT_ROOT/cltest/_test_input.txt" 2>&1 | grep -q "invalid option"
-[ $? -eq 1 ]
