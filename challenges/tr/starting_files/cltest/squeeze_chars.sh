a3817114237b3583a82e77564343255f2ff7ce9f
Sandro Hawke <sandro@hawke.org>
2024-11-28 11:19:06 -0500

    remove too-hard tr tests

--- challenges/tr/starting_files/cltest/squeeze_chars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source "$PROJECT_ROOT/cltest/_common.sh"
-
-# Test squeezing repeated characters
-echo "hello    there" | $COMMAND -s '[:space:]' > output.txt
-assert_output "hello there" "$(cat output.txt)"
