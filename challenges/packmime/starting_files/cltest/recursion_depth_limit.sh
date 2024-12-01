b61f69d3b30b3cad919a176aacf26ba1916e4010
Sandro Hawke <sandro@hawke.org>
2024-11-30 23:50:01 -0500

    chmod +x, and tweak recursiion limit

--- challenges/packmime/starting_files/cltest/recursion_depth_limit.sh
+++ challenges/packmime/starting_files/cltest/recursion_depth_limit.sh
@@ -6,20 +6,20 @@ dir="test"
 for i in {1..32}; do
     dir="$dir/subdir"
     mkdir -p "$dir"
-    echo "level $i" > "$dir/file.txt"
 done
+echo "deep file" "$dir/file.txt"
 
 # This should succeed
 $COMMAND test > out.ok
-assert [ $? -eq 0 ]
 
 # Now add one more level to exceed limit
 mkdir -p "$dir/subdir"
 echo "too deep" > "$dir/subdir/file.txt"
 
 # This should fail with recursion depth error
-$COMMAND test 2> err.fail
-assert [ $? -ne 0 ]
-assert grep -q "recursion.*32" err.fail
+if $COMMAND test 2> err.fail; then
+    assert false
+fi
+assert grep -q "recur" err.fail
 
 end_of_test
